2fd77a89648818418034e863b9f4133b86e428d4
Mike Goldsmth <goldsmith.mike@gmail.com>
2021-09-29 14:10:36 +0100

    build and publish multi-arch docker images on tag

---
+++ build/build_docker.sh
@@ -0,0 +1,20 @@
+set -o nounset
+set -o pipefail
+
+VERSION="${VERSION:-dev}"
+TAGS="latest"
+if [[ $VERSION != "dev" ]]; then
+    TAGS+=",$VERSION"
+fi
+
+unset GOOS
+unset GOARCH
+export KO_DOCKER_REPO=${KO_DOCKER_REPO:-ko.local}
+export GOFLAGS="-ldflags=-X=main.BuildID=$VERSION"
+export SOURCE_DATE_EPOCH=$(date +%s)
+# shellcheck disable=SC2086
+ko publish \
+  --tags "${TAGS}" \
+  --base-import-paths \
+  --platform "linux/amd64,linux/arm64" \
+  .
